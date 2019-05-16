d7ce1844efec16336e91e038fd2b2c98cf830f5d
riogod <riogod@gmail.com>
2019-05-16 18:17:32 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createStore, bindActionCreators } from 'redux';
+
+import reducer from './reducer';
+import * as actions from './action';
+
+const store = createStore(reducer);
+const { dispatch } = store;
+
+
+const { inc, dec, rnd } =
+    bindActionCreators(actions, dispatch);
 
 
 
 
+document.getElementById('inc')
+    .addEventListener('click', inc);
 
-ReactDOM.render(<h1>Hi!</h1>
-    , document.getElementById('root'));
+document.getElementById('dec')
+    .addEventListener('click', dec);
+
+document.getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random()*10);
+        rnd(payload);
+    });
 
 
+const update = () => {
+    document
+        .getElementById('counter')
+        .innerHTML = store.getState();
+}
+
+store.subscribe(update);
